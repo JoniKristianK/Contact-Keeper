99f8e3ad40922ced1dd19314e2a93ba9590a3abc
JoniKristianK <joni.karilainen@gmail.com>
2020-11-23 12:09:28 +0200

    Create & Respond with JSON Web Token

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
-const { json } = require('express');
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -48,7 +49,23 @@ router.post(
 
       await user.save();
 
-      res.send('User saved');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw error;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');

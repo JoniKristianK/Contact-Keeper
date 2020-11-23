ca1c02de527506322f39405202d571840bfeb43b
JoniKristianK <joni.karilainen@gmail.com>
2020-11-23 14:07:24 +0200

    Auth Middleware & Protecting Routes

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const UserSchema = mongoose.Schema({
     type: String,
     required: true,
   },
-  name: {
+  email: {
     type: String,
     required: true,
     unique: true,
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -10,8 +11,14 @@ const User = require('../models/User');
 // @router      GET /api/auth
 // @desc        Get logged in user
 // @access      Private
-router.get('/', (req, res) => {
-  res.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @router      POST /api/auth
@@ -41,7 +48,7 @@ router.post(
       const isMatch = await bcrypt.compare(password, user.password);
 
       if (!isMatch) {
-        return res.status(400).json({ msg: 'Invalid Credentials pss' });
+        return res.status(400).json({ msg: 'Invalid Credentials psw' });
       }
 
       const payload = {

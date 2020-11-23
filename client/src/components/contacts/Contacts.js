0f990adb4ce4075b0310572757d9aa4f329533d5
JoniKristianK <joni.karilainen@gmail.com>
2020-11-23 20:25:13 +0200

    Contacts & ContactItem Components

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+
+import ContactItem from './ContactItem';
+
+import ContactContext from '../../context/contact/contactContext';
+
+export const Contacts = () => {
+  const contactContext = useContext(ContactContext);
+
+  const { contacts } = contactContext;
+
+  return (
+    <>
+      {contacts.map((contact) => (
+        <ContactItem key={contact.id} contact={contact} />
+      ))}
+    </>
+  );
+};
+
+export default Contacts;

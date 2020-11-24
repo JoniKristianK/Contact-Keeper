f0378b67e2db9a5ace4c9563c4e636ce6ebf4369
JoniKristianK <joni.karilainen@gmail.com>
2020-11-24 21:24:41 +0200

    Get & Clear Contacts

--- client/src/components/layout/NavBar.js
+++ client/src/components/layout/NavBar.js
@@ -3,14 +3,18 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import AuthContext from '../../context/auth/authContext';
+import ContactContext from '../../context/contact/contactContext';
 
 const NavBar = ({ title, icon }) => {
   const authContext = useContext(AuthContext);
+  const contactContext = useContext(ContactContext);
 
   const { isAuthenticated, logout, user } = authContext;
+  const { clearContacts } = contactContext;
 
   const onLogout = () => {
     logout();
+    clearContacts();
   };
 
   const authLinks = (
--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -1,10 +1,12 @@
 import {
+  GET_CONTACTS,
   ADD_CONTACT,
   DELETE_CONTACT,
   SET_CURRENT,
   CLEAR_CURRENT,
   UPDATE_CONTACT,
   FILTER_CONTACTS,
+  CLEAR_CONTACTS,
   CLEAR_FILTER,
   CONTACT_ERROR,
 } from '../types';
@@ -12,10 +14,17 @@ import {
 // eslint-disable-next-line
 export default (state, action) => {
   switch (action.type) {
+    case GET_CONTACTS:
+      return {
+        ...state,
+        contacts: action.payload,
+        loading: false,
+      };
     case ADD_CONTACT:
       return {
         ...state,
         contacts: [...state.contacts, action.payload],
+        loading: false,
       };
     case DELETE_CONTACT:
       return {
@@ -23,16 +32,27 @@ export default (state, action) => {
         contacts: state.contacts.filter(
           (contact) => contact.id !== action.payload
         ),
+        loading: false,
+      };
+    case CLEAR_CONTACTS:
+      return {
+        ...state,
+        contacts: null,
+        filtered: null,
+        error: null,
+        current: null,
       };
     case SET_CURRENT:
       return {
         ...state,
         current: action.payload,
+        loading: false,
       };
     case CLEAR_CURRENT:
       return {
         ...state,
         current: null,
+        loading: false,
       };
     case UPDATE_CONTACT:
       return {
@@ -40,6 +60,7 @@ export default (state, action) => {
         contacts: state.contacts.map((contact) =>
           contact.id === action.payload.id ? action.payload : contact
         ),
+        loading: false,
       };
     case FILTER_CONTACTS:
       return {
@@ -48,16 +69,19 @@ export default (state, action) => {
           const regex = new RegExp(`${action.payload}`, 'gi');
           return contact.name.match(regex) || contact.email.match(regex);
         }),
+        loading: false,
       };
     case CLEAR_FILTER:
       return {
         ...state,
         filtered: null,
+        loading: false,
       };
     case CONTACT_ERROR:
       return {
         ...state,
         error: action.payload,
+        loading: false,
       };
 
     default:

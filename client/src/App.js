bfaeb4ef2280326e7bb444298e700eb3aa40a340
JoniKristianK <joni.karilainen@gmail.com>
2020-11-24 15:04:18 +0200

    Alert Context, State & Component

--- client/src/App.js
+++ client/src/App.js
@@ -6,28 +6,33 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
 import './App.css';
 
 import AuthState from './context/auth/AuthState';
 import ContactState from './context/contact/ContactState';
+import AlertState from './context/alert/AlertState';
 
 const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <>
-            <NavBar />
-            <div className='container'>
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/about' component={About} />
-                <Route exact path='/register' component={Register} />
-                <Route exact path='/login' component={Login} />
-              </Switch>
-            </div>
-          </>
-        </Router>
+        <AlertState>
+          <Router>
+            <>
+              <NavBar />
+              <div className='container'>
+                <Alerts />
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
+            </>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
